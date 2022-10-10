c907490bb633af1fa9ff136e5f18fdecd2a3039c
John Deranian <fun@scooby.cc>
2022-10-09 23:23:03 -0300

    Initial Commit

---
+++ M5WrapUpChallenge/M5WrapUpChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M5WrapUpChallengeApp.swift
+//  M5WrapUpChallenge
+//
+//  Created by John Deranian on 10/9/22.
+//
+
+import SwiftUI
+
+@main
+struct M5WrapUpChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
