34749b0ad3dfe2684ca75f2bff3045a8d12b224a
John Deranian <fun@scooby.cc>
2022-10-10 08:12:55 -0300

    add url to videos and VideoPlayer going to host the videos locally so I can test without the internet .

--- M5WrapUpChallenge/Views/LessonDetailView.swift
+++ M5WrapUpChallenge/Views/LessonDetailView.swift
@@ -6,21 +6,38 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct LessonDetailView: View {
+    @EnvironmentObject var lessonModel: LessonModel
     
     var lesson: Lesson
+    
     var body: some View {
-        Text(lesson.url)
+        
+        VStack{
+            if let url = URL(string: lesson.url) {
+                VideoPlayer(player: AVPlayer(url: url))
+                    .cornerRadius(10)
+                    .frame(maxHeight: 300)
+            }
+            Button("Return to Home"){
+                lessonModel.gotoHomePage()
+            }
+        }
+        //Text(lesson.url)
     }
 }
 
-/*
+
 struct LessonDetailView_Previews: PreviewProvider {
     static var previews: some View {
+        let lessons = LessonModel()
+        //let model = AuthorModel()
+        //DetailView(thisauthor: model.authors[0])
         
-        var lesson = Lesson
-        LessonDetailView(lesson: <#T##Lesson#>)
+        var lesson = lessons.lessons[1]
+        LessonDetailView(lesson: lesson)
     }
 }
-*/
+
